6e7c95cfa7a6ca1bb3f297d30b9fad4a35b0a3ca
wahid <wahid@DESKTOP-JLU265U>
2022-01-25 23:22:11 -0800

    Add project files.

---
+++ ProductSearch/DBModule.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSearch
+{
+    public class DBModule
+    {
+        private SqlConnection con;
+        private SqlCommand cmd;
+        private SqlDataAdapter da;
+
+        public DBModule()
+        {
+            con = new SqlConnection();
+            cmd = new SqlCommand();
+            da = new SqlDataAdapter();
+        }
+
+        public void StartConnection()
+        {
+            String connection = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            con = new SqlConnection(connection);
+            con.Open();
+        }
+
+        public DataTable FillDataTable()
+        {
+            StartConnection();
+            DataTable dtCurrency = new DataTable();
+            cmd = new SqlCommand("select ProductId, SKU, Title, Artist from Product", con);
+            cmd.CommandType = CommandType.Text;
+            da = new SqlDataAdapter(cmd);
+            da.Fill(dtCurrency);
+            con.Close();
+
+            return dtCurrency;
+
+        }
+
+        public void UpdateDB(string id, string sku, string artist, string title)
+        {
+            StartConnection();
+            cmd = new SqlCommand("UPDATE Product SET SKU = @sku, Title = @title, Artist = @artist, ModifiedDate = @md WHERE ProductId = @id", con);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@sku", sku);
+            cmd.Parameters.AddWithValue("@title", title);
+            cmd.Parameters.AddWithValue("@artist", artist);
+            cmd.Parameters.AddWithValue("@md", SqlDbType.DateTime).Value = DateTime.Now;
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public DataTable SearchDB(string SKU, string artist, string title)
+        {
+            StartConnection();
+            DataTable dtCurrency = new DataTable();
+            cmd = new SqlCommand("SearchTable", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            if (!title.Equals(string.Empty) && title != null)
+            {
+                cmd.Parameters.AddWithValue("@sTitle", SqlDbType.VarChar).Value = title;
+            }
+            if (!SKU.Equals(string.Empty) && SKU != null)
+            {
+                cmd.Parameters.AddWithValue("@sSKU", SqlDbType.VarChar).Value = SKU;
+            }
+            if (!artist.Equals(string.Empty) && artist != null)
+            {
+                cmd.Parameters.AddWithValue("@sArtist", SqlDbType.VarChar).Value = artist;
+            }
+
+            da = new SqlDataAdapter(cmd);
+            da.Fill(dtCurrency);
+            con.Close();
+
+            return dtCurrency;
+        }
+    }
+}
---
+++ ProductSearch/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProductSearch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string sku;
+        private string artist;
+        private string title;
+        private string productId;
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillDataGrid();
+        }
+
+        public void FillDataGrid()
+        {
+            DataTable dtProduct = new DataTable();
+            DBModule dBModule = new DBModule();
+            dtProduct = dBModule.FillDataTable();
+
+            if (dtProduct != null && dtProduct.Rows.Count > 0)
+            {
+                dtGrid.ItemsSource = dtProduct.DefaultView;
+            }
+            else
+            {
+                dtGrid.ItemsSource = null;
+            }
+        }
+
+        private void searchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string SKU = txtSKU.Text.Trim();    
+            string artist = txtArtisit.Text.Trim().ToUpper();
+            string title = txtTitlle.Text.Trim().ToUpper();
+
+            DataTable dtProduct = new DataTable();
+            DBModule dBModule = new DBModule();
+            dtProduct = dBModule.SearchDB(SKU, artist, title);
+            dtGrid.ItemsSource = dtProduct.DefaultView;
+
+        }
+
+        private void Row_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            DataGridRow row = (DataGridRow)sender;
+            DataRowView drv = (DataRowView)row.Item;
+            
+            productId = drv[0].ToString();
+            sku = drv[1].ToString();
+            title = drv[2].ToString();
+            artist = drv[3].ToString();
+
+            txtSKU.Text = sku;
+            txtArtisit.Text = artist;
+            txtTitlle.Text = title;
+
+            searchBtn.Visibility = Visibility.Hidden;
+            saveBtn.Visibility = Visibility.Visible;
+            cancelBtn.Visibility = Visibility.Visible;
+
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if ( (txtSKU.Text.Trim().Equals(String.Empty)) || 
+                (txtArtisit.Text.Trim().Equals(String.Empty)) || (txtTitlle.Text.Trim().Equals(String.Empty)))
+            {
+                MessageBox.Show("There are empty fields", "Information", MessageBoxButton.OK, MessageBoxImage.Hand);
+                return;
+            }
+
+            if ( (txtSKU.Text.Trim().ToUpper().Equals(sku.ToUpper())) && (txtArtisit.Text.Trim().ToUpper().Equals(artist.ToUpper())) 
+                && (txtTitlle.Text.Trim().ToUpper().Equals(title.ToUpper())) )
+            {
+                MessageBox.Show("No changes were made", "Information", MessageBoxButton.OK, MessageBoxImage.Hand);
+                return;
+            }
+
+            string skuToSave = txtSKU.Text.Trim();
+            string artistToSave = txtArtisit.Text.Trim();
+            string titleToSave = txtTitlle.Text.Trim();
+
+            DBModule dBModule = new DBModule();
+            dBModule.UpdateDB(productId, skuToSave, artistToSave, titleToSave);
+            MessageBox.Show("Table updated", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            
+            searchBtn.Visibility = Visibility.Visible;
+            saveBtn.Visibility = Visibility.Hidden;
+            cancelBtn.Visibility = Visibility.Hidden;
+
+            txtSKU.Clear();
+            txtArtisit.Clear();
+            txtTitlle.Clear();
+            
+            FillDataGrid();
+
+        }
+
+        private void cancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            txtSKU.Clear();
+            txtArtisit.Clear();
+            txtTitlle.Clear();
+
+            searchBtn.Visibility = Visibility.Visible;
+            saveBtn.Visibility = Visibility.Hidden;
+            cancelBtn.Visibility = Visibility.Hidden;
+        }
+    }
+}
